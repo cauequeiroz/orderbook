ff73be64ed8ae5e95df99f076602fb8f12540217
Caue Queiroz <cauenqueiroz@gmail.com>
2017-09-13 20:10:50 -0300

    Change port to deploy

--- _server/server.js
+++ _server/server.js
@@ -1,9 +1,10 @@
 /* Código simplório, apenas para fornecer o serviço para a aplicação */
 
 var http = require('http')
-    ,app = require('./config/express');
+    ,app = require('./config/express')
+    ,port = process.env.PORT || 3000;
 
-http.createServer(app).listen(3000, function() {
+http.createServer(app).listen(port, function() {
     console.log('\n\n====================================================');
     console.log('Listen on port: ' + this.address().port);
 });
